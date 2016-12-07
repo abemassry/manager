b6109e7c20f9515d88192c7158cb3de532688aa2
Abe Massry <amassry@linode.com>
2016-12-07 16:00:55 -0500

    remove additional test description

--- test/api/gen.spec.js
+++ test/api/gen.spec.js
@@ -3,27 +3,25 @@ import { expect } from 'chai';
 import * as gen from '../../src/api/gen';
 
 describe('api/gen', () => {
-  describe('verfiyGenConfig', () => {
-    const sandbox = sinon.sandbox.create();
+  const sandbox = sinon.sandbox.create();
 
-    afterEach(() => {
-      sandbox.restore();
-    });
+  afterEach(() => {
+    sandbox.restore();
+  });
 
-    it('should look like a datacenter config', () => {
-      const config = gen.genConfig({
-        plural: 'datacenters',
-        singular: 'datacenter',
-        localStorageCacheable: true,
-        endpoint: id => `/datacenters/${id}`,
-        supports: [gen.ONE, gen.MANY],
-      });
-      expect(config.plural).to.equal('datacenters');
-      expect(config.singular).to.equal('datacenter');
-      expect(config.localStorageCacheable).to.equal(true);
-      expect(config.supports).to.deep.equal(['ONE', 'MANY']);
-      expect(config.parent).to.equal(undefined);
+  it('should look like a datacenter config', () => {
+    const config = gen.genConfig({
+      plural: 'datacenters',
+      singular: 'datacenter',
+      localStorageCacheable: true,
+      endpoint: id => `/datacenters/${id}`,
+      supports: [gen.ONE, gen.MANY],
     });
+    expect(config.plural).to.equal('datacenters');
+    expect(config.singular).to.equal('datacenter');
+    expect(config.localStorageCacheable).to.equal(true);
+    expect(config.supports).to.deep.equal(['ONE', 'MANY']);
+    expect(config.parent).to.equal(undefined);
   });
 
   it('should generate a default state', () => {
